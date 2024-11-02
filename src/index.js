929169610d52c7f03f18b19b8c9e1cdb311a29c6
huyvanvu2003 <huyvanvu2003@gmail.com>
2024-11-02 19:23:13 +0700

    hoàn thành 70% trang login và signup + quá trình chỉnh sửa chi tiết các trang nhỏ. ( còn lại test và chỉnh sửa cho 30% còn lại)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//import component
+import LoginIndex from './component/views/login.view';
+import SignUpIndex from './component/views/signup.view';
+
+// style
+
+import "./component/assets/css/bootstrap.min.css";
+import "./component/assets/css/now-ui-kit.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<LoginIndex />} />
+      <Route path="/login" element={<LoginIndex />} />
+      <Route path="/signup" element={<SignUpIndex />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </BrowserRouter>
+);
