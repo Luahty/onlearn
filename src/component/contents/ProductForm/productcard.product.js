ba311109ad672c22b4706cbc5a6fe1819f10c1da
lenhatduyit <lenhatduyit@gmail.com>
2024-11-26 13:03:35 +0700

    commit

--- src/component/contents/ProductForm/productcard.product.js
+++ src/component/contents/ProductForm/productcard.product.js
@@ -1,21 +1,14 @@
 import React from "react";
-import '../Css/product.css'; 
+import "../Css/product.css";
 
 const ProductCard = ({ product }) => {
-  return (
-    <div className="product">
-      <i className="far fa-heart icon-heart"></i>
-      <img src={product.image} alt={product.name} />
-      <div className="details">
-        <h4>{product.name}</h4>
-        <p className="bakery">{product.bakery}</p>
-        <p className="price">{product.price}</p>
-        <div className="favorite">
-          <i className="far fa-heart"></i>
+    return (
+        <div className="product-card border p-4 rounded shadow">
+            <img src={product.topic_avatar} alt={product.name} className="w-full h-40 object-cover rounded mb-2" />
+            <h2 className="text-xl font-bold">{product.name}</h2>
+            <p className="text-gray-600">{product.level}</p>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductCard;
